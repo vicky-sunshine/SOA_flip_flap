5352d598c5cbc4661137f3343363f4f5abb4a8bb
vicky <lch82327@gmail.com>
2015-10-02 13:17:19 +0800

    done

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,15 @@
 require_relative 'tsv_buddy'
+require 'yaml'
 
 class FlipFlap
   # Do NOT create an initialize method
+  include TsvBuddy
 
+  def take_yaml(yml)
+    @data = YAML.load(yml)
+  end
+
+  def to_yaml
+    @data.to_yaml
+  end
 end
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -7,12 +7,43 @@ module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    lines = []
+    tsv.each_line { |line| lines << line}
 
+    # get keys
+    keys = lines[0].chop.split("\t")
+    # shift out the keys
+    lines.shift
+
+    # get values and put into yml stucture
+    format_data = []
+    lines.each do |line|
+      element = {}
+      values = line.chop.split("\t")
+      keys.each_index { |index| element[keys[index]] = values[index] }
+      format_data.push(element)
+    end
+    @data = format_data
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    format_string = ""
+    keys = @data[0].keys
+    format_string << keys[0]
+    keys.shift
+    keys.each { |key| format_string <<  "\t" + key}
+    format_string << "\n"
 
+    # print value
+    @data.each do |element|
+      # avoid "\t" at the tail of line
+      format_string << element.values[0]
+      element.shift
+      element.each_value {|value| format_string << "\t" + value}
+      format_string << "\n"
+    end
+    format_string
   end
 end
